d2793ba4b7c1b1593198ddeb16bd34f53fab026d
xkwd <34714644+xkwd@users.noreply.github.com>
2024-03-12 10:21:18 +0100

    Add benchmarks

--- README.md
+++ README.md
@@ -14,3 +14,16 @@ OXML is a fast Ruby XML to Hash parser, built on top of the [Ox](https://github.
 
   OXML.parse(xml, options)
 ```
+
+### Benchmarks
+
+```
+XML -> Hash:
+                           user     system      total        real
+xmlsimple                  0.477219   0.013127   0.490346 (  0.490372)
+activesupport w/ rexml     0.434693   0.005745   0.440438 (  0.440469)
+nori                       0.108569   0.008505   0.117074 (  0.117098)
+activesupport w/ nokogiri  0.065717   0.001351   0.067068 (  0.067069)
+activesupport w/ libxml    0.052466   0.000904   0.053370 (  0.053374)
+oxml                       0.030874   0.000326   0.031200 (  0.031203)
+```
---
+++ benchmarks/script.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'benchmark'
+require 'oxml'
+require 'nori'
+require 'xmlsimple'
+require 'active_support/core_ext/hash/conversions'
+
+xml = File.read("#{__dir__}/sample.xml")
+
+puts 'XML -> Hash:'
+runs = 100
+
+Benchmark.bm 20 do |x|
+  x.report 'xmlsimple                ' do
+    runs.times { XmlSimple.xml_in(xml) }
+  end
+
+  ActiveSupport::XmlMini.backend = 'REXML'
+  x.report 'activesupport w/ rexml   ' do
+    runs.times { Hash.from_xml(xml) }
+  end
+
+  x.report 'nori                     ' do
+    runs.times { Nori.new.parse(xml) }
+  end
+
+  ActiveSupport::XmlMini.backend = 'Nokogiri'
+  x.report 'activesupport w/ nokogiri' do
+    runs.times { Hash.from_xml(xml) }
+  end
+
+  ActiveSupport::XmlMini.backend = 'LibXML'
+  x.report 'activesupport w/ libxml  ' do
+    runs.times { Hash.from_xml(xml) }
+  end
+
+  x.report 'oxml                     ' do
+    runs.times { OXML.parse(xml) }
+  end
+end
